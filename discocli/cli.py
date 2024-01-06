1947580b67771023100a34b84d833b0c19fb8692
Antoine Leclair <antoineleclair@gmail.com>
2024-01-05 21:03:36 -0500

    Add basic init SSH command
    That will download and run the install script.

--- discocli/cli.py
+++ discocli/cli.py
@@ -1,11 +1,13 @@
 import click
 
+
+
 @click.group()
 @click.version_option(
     package_name="disco-cli",
     message="%(package)s %(version)s",
 )
-def main():
+def main() -> None:
     pass
 
 @main.command()
@@ -14,5 +16,13 @@ def main():
     required=True,
     help="user@host, e.g. root@123.123.123.123",
 )
-def init(ssh: str):
-    click.echo(f"Would connect to {ssh} and init.")
+@click.option(
+    "--verbose",
+    default=False,
+    show_default=True,
+    help="Make the operation more talkative",
+)
+def init(ssh: str, verbose: bool) -> None:
+    from discocli.commands.init import init_command
+
+    init_command(ssh=ssh, verbose=verbose)
---
+++ discocli/commands/init.py
@@ -0,0 +1,38 @@
+import subprocess
+
+import click
+
+def _ssh_command(connection_str: str, command: str, verbose: bool) -> tuple[bool, str]:
+    try:
+        args = [
+            "ssh",
+            "-vvv",
+            connection_str,
+            "bash -s",
+        ]
+        if not verbose:
+            args.remove("-vvv")
+        result = subprocess.run(
+            args=args,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            input=command.encode("utf-8"),
+        )
+    except subprocess.CalledProcessError as ex:
+        return False, ex.stdout.decode("utf-8")
+    return True, result.stdout.decode("utf-8")
+
+
+def init_command(ssh: str, verbose: bool) -> None:
+    click.echo(f"Installing Disco on {ssh}")
+    command = "curl https://downloads.letsdisco.dev/latest/init | sudo sh"
+    success, output = _ssh_command(connection_str=ssh, command=command, verbose=verbose)
+    if not success:
+        click.echo("Failed")
+        click.echo(output)
+        return
+    else:
+        click.echo("Success")
+        click.echo(output)
+        return
