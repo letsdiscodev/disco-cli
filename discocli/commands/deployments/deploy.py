ebf63dfa0026af94eb586509e0436d3e3eb362c7
Antoine Leclair <antoineleclair@gmail.com>
2024-01-19 22:41:56 -0500

    Multiple services per project

--- discocli/commands/deployments/deploy.py
+++ discocli/commands/deployments/deploy.py
@@ -10,22 +10,32 @@ from discocli import config
     help="the name that you'll use to refer to the project",
 )
 @click.option(
-    "--image",
+    "--commit",
     required=False,
-    help="the name of the image that you want to deploy, ""if you're deployment an image",
+    help="the commit to deploy, e.g. 7b5c8f935328c1af49c9037cac9dee7bf0bd8c7e",
+)
+@click.option(
+    "--file",
+    required=False,
+    help="the JSON file to deploy",
 )
 @click.option(
     "--disco-domain",
     required=False,
     help="The domain where Disco is running",
 )
-def deploy(name: str, image: str, disco_domain: str | None) -> None:
+def deploy(name: str, commit: str, file: str, disco_domain: str | None) -> None:
     disco_domain_config = config.get_disco_domain(disco_domain)
     disco_domain = disco_domain_config["domain"]
     click.echo(f"Deploying {name}")
     url = f"https://{disco_domain}/projects/{name}/deployments"
+    disco_config = None
+    if file is not None:
+        with open(file, "r", encoding="utf-8") as f:
+            disco_config = f.read()
     req_body = dict(
-        image=image,
+        commit=commit,
+        discoConfig=disco_config,
     )
     response = requests.post(url,
         json=req_body,
