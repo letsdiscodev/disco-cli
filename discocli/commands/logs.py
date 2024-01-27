ba21bab06b985fabe76ba9c4d933923694d674ee
Antoine Leclair <antoineleclair@gmail.com>
2024-01-27 13:54:43 -0500

    Filter logs for project and service

--- discocli/commands/logs.py
+++ discocli/commands/logs.py
@@ -6,14 +6,30 @@ from discocli import config
 
 @click.command(name="logs")
 @click.option(
+    "--project",
+    required=False,
+    help="The project you want to get logs about",
+)
+@click.option(
+    "--service",
+    required=False,
+    help="The service you want to get logs about",
+)
+@click.option(
     "--disco-domain",
     required=False,
     help="The domain where Disco is running",
 )
-def logs(disco_domain: str | None) -> None:
+def logs(disco_domain: str | None, project: str | None, service: str | None) -> None:
     disco_domain_config = config.get_disco_domain(disco_domain)
     disco_domain = disco_domain_config["domain"]
+    if project is None and service is not None:
+        raise Exception("Must specify project when specifying service")
     url = f"wss://{disco_domain}/logs"
+    if project is not None:
+        url = f"{url}/{project}" 
+    if service is not None:
+        url = f"{url}/{service}" 
     with connect(url) as websocket:
         while True:
             log_json_str = websocket.recv()
