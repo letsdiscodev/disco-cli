34387a713c858b64f96a1d16d5dd303ad7be7ede
Antoine Leclair <antoineleclair@gmail.com>
2024-03-16 16:32:32 -0400

    Add output of latest build

--- discocli/commands/deployments/output.py
+++ discocli/commands/deployments/output.py
@@ -15,8 +15,8 @@ from discocli import config
 )
 @click.option(
     "--deployment",
-    required=True,
-    help="the deployment number",
+    default=0,
+    help="the deployment number, defaults to latest",
 )
 @click.option(
     "--disco",
@@ -35,6 +35,9 @@ def deploy_output(
         stream=True,
         verify=config.requests_verify(disco_config),
     )
+    if response.status_code == 404:
+        click.echo("No deployment found")
+        return
     for event in sseclient.SSEClient(response).events():
         output = json.loads(event.data)
         click.echo(output["text"], nl=False)
