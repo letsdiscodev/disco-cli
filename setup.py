f5a4fc42c094197db242e5d8b41b451c8d33e490
Antoine Leclair <antoineleclair@gmail.com>
2024-04-03 18:55:53 -0400

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='disco-cli',
-    version="0.3.0.dev1",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
